a0dcc04d8ccf1d5b6f39ebf04086881133181c11
cingo85 <cingo85@gmail.com>
2019-08-30 17:35:19 +0800

    jason
    wip

--- src/main/java/tw/com/leadteketl/bean/t_column_master.java
+++ src/main/java/tw/com/leadteketl/bean/t_column_master.java
@@ -74,8 +74,130 @@ public class t_column_master implements java.io.Serializable{
 	@Column(name="column_length",length=100)
 	public Integer column_length;
 	
+	/*
+	 * 0827
+	 * */
+	
+	@ApiModelProperty(value = "欄位在表內型態", required = true)
+	@Column(name="columnInTableType",length=100)
+	public String columnInTableType;
+	
 	@ManyToOne
 	@JoinColumn(name="t_column_master_table_id",referencedColumnName = "table_id")
 	public t_table_master t_table_master;
+
+	public Integer getSn() {
+		return sn;
+	}
+
+	public void setSn(Integer sn) {
+		this.sn = sn;
+	}
+
+	public String getTableId() {
+		return tableId;
+	}
+
+	public void setTableId(String tableId) {
+		this.tableId = tableId;
+	}
+
+	public String getColumn_id() {
+		return column_id;
+	}
+
+	public void setColumn_id(String column_id) {
+		this.column_id = column_id;
+	}
+
+	public String getColumn_name() {
+		return column_name;
+	}
+
+	public void setColumn_name(String column_name) {
+		this.column_name = column_name;
+	}
+
+	public String getColumn_read_name() {
+		return column_read_name;
+	}
+
+	public void setColumn_read_name(String column_read_name) {
+		this.column_read_name = column_read_name;
+	}
+
+	public String getColumn_c_name() {
+		return column_c_name;
+	}
+
+	public void setColumn_c_name(String column_c_name) {
+		this.column_c_name = column_c_name;
+	}
+
+	public boolean isIs_pk() {
+		return is_pk;
+	}
+
+	public void setIs_pk(boolean is_pk) {
+		this.is_pk = is_pk;
+	}
+
+	public String getColumn_type() {
+		return column_type;
+	}
+
+	public void setColumn_type(String column_type) {
+		this.column_type = column_type;
+	}
+
+	public String getColumn_default() {
+		return column_default;
+	}
+
+	public void setColumn_default(String column_default) {
+		this.column_default = column_default;
+	}
+
+	public boolean isIs_datamodel_attribute() {
+		return is_datamodel_attribute;
+	}
+
+	public void setIs_datamodel_attribute(boolean is_datamodel_attribute) {
+		this.is_datamodel_attribute = is_datamodel_attribute;
+	}
+
+	public String getColumnMasterState() {
+		return ColumnMasterState;
+	}
+
+	public void setColumnMasterState(String columnMasterState) {
+		ColumnMasterState = columnMasterState;
+	}
+
+	public Integer getColumn_length() {
+		return column_length;
+	}
+
+	public void setColumn_length(Integer column_length) {
+		this.column_length = column_length;
+	}
+
+	public String getColumnInTableType() {
+		return columnInTableType;
+	}
+
+	public void setColumnInTableType(String columnInTableType) {
+		this.columnInTableType = columnInTableType;
+	}
+
+	public t_table_master getT_table_master() {
+		return t_table_master;
+	}
+
+	public void setT_table_master(t_table_master t_table_master) {
+		this.t_table_master = t_table_master;
+	}
+	
+	
 		
 }
